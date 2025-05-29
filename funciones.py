b41fc17e7d11c557a97cffd444fd171c26da347f
JMyers54 <juandiegoquintero455@gmail.com>
2025-05-29 16:20:01 -0500

    se agrego nueva funcíon y se solucionaron errores posteriores a la creación de la función

--- funciones.py
+++ funciones.py
@@ -5,12 +5,32 @@ import pygame.mixer as mx
 
 
 class Funciones():
-    def __init__(self,estado,):
+    def __init__(self, estado, btnPausa, btnStop, btnResume, btnPlay):
         self.estado = estado
-
+        self.btnPausa = btnPausa
+        self.btnStop = btnStop
+        self.btnResume = btnResume
+        self.btnPlay = btnPlay
+        self.canciónActual = r"sounds\Pista.mp3"
+        mx.music.load(self.canciónActual)
+        self.pausado = False
+    
+    def archivoMP3(self, event=None):
+        archivo = filedialog.askopenfilename(title="Abrir archivo de música",filetypes=[("archivos MP3","*.mp3")])
+        if archivo:
+            try:
+                mx.music.load(archivo)
+                mx.music.play()
+                self.estado.config(text=f"Reproduciendo:{archivo.split('/')[-1]}")
+            except Exception as e:
+                self.estado.config(text=f"Error la reproducir: {e}")
     def play(self, event):
-        mx.music.load(r"sounds\Pista.mp3")
-        mx.music.play()
+        if self.pausado:
+            mx.music.unpause()
+            self.pausado = False
+        else:
+            mx.music.play()
+
         self.estado.config(text="Reproduciendo...")
         self.btnPausa.config(state="normal")
         self.btnStop.config(state="normal")
@@ -19,6 +39,7 @@ class Funciones():
     
     def stop(self, event):
         mx.music.stop()
+        self.pausado = False
         self.estado.config(text="Reproducción detenida...")
         self.btnPlay.config(state="normal")
         self.btnPausa.config(state="disabled")
@@ -27,6 +48,7 @@ class Funciones():
 
     def pausa(self, event):
         mx.music.pause()
+        self.pausado = True
         self.estado.config(text="Reproducción pausada...")
 
     def resume(self, event):
