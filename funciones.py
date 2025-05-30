07605691b3fb4848828fdc36f3501ee19d258855
Jcamilo700 <juanca70087g@gmail.com>
2025-05-30 02:31:45 -0600

    nd

---
+++ funciones.py
@@ -0,0 +1,70 @@
+from tkinter import PhotoImage, filedialog,ttk
+import random
+import mutagen
+import pygame.mixer as mx
+import tkinter.ttk as ttk
+from mutagen.mp3 import MP3
+import time
+
+class Funciones():
+    def __init__(self, estado, btnPausa, btnStop, btnResume, btnPlay, progreso):
+        self.estado = estado
+        self.btnPausa = btnPausa
+        self.rutaActual = r"sounds\Pista.mp3"
+        self.btnStop = btnStop
+        self.btnResume = btnResume
+        self.btnPlay = btnPlay
+        self.progreso = progreso
+        self.canciónActual = r"sounds\Pista.mp3"
+        mx.music.load(self.canciónActual)
+        self.pausado = False
+    
+    def archivoMP3(self, event=None):
+        archivo = filedialog.askopenfilename(title="Abrir archivo de música",filetypes=[("archivos MP3","*.mp3")])
+        if archivo:
+            try:
+                mx.music.load(archivo)
+                mx.music.play()
+                self.estado.config(text=f"Reproduciendo:{archivo.split('/')[-1]}")
+            except Exception as e:
+                self.estado.config(text=f"Error la reproducir: {e}")
+    def play(self, event):
+        if self.pausado:
+            mx.music.unpause()
+            self.pausado = False
+        else:
+            mx.music.play()
+            audio = MP3(self.rutaActual)
+            self.duracion = int(audio.info.length)
+            self.progreso.config(to=self.duracion)
+            self.BarraDeProgreso()
+
+        self.estado.config(text="Reproduciendo...")
+        self.btnPausa.config(state="normal")
+        self.btnStop.config(state="normal")
+        self.btnResume.config(state="normal")
+        self.btnPlay.config(state="disabled")
+    
+    def stop(self, event):
+        mx.music.stop()
+        self.pausado = False
+        self.estado.config(text="Reproducción detenida...")
+        self.btnPlay.config(state="normal")
+        self.btnPausa.config(state="disabled")
+        self.btnStop.config(state="disabled")
+        self.btnResume.config(state="disabled")
+
+    def pausa(self, event):
+        mx.music.pause()
+        self.pausado = True
+        self.estado.config(text="Reproducción pausada...")
+
+    def resume(self, event):
+        mx.music.unpause()
+        self.estado.config(text="Reproduciendo...")
+
+    def BarraDeProgreso(self):
+        if mx.music.get_busy():
+            pos = mx.music.get_pos() // 1000
+            self.progreso.set(pos)
+            self.estado.after(1000, self.BarraDeProgreso)
